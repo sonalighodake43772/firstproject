d178e4c407063c23eeb0ba35d88fc1e1de4d307d
usha867 <113333875+usha867@users.noreply.github.com>
2022-09-12 12:57:09 +0530

    swapping variable

---
+++ swap.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author s
+ */
+public class swap {
+    public static void main(String[] args) {
+        int a=10;
+        int b=15;
+        System.out.println("before swapping-");
+        System.out.println(a);
+        System.out.println(b);
+        
+        int temp=a;
+        a=b;
+        b=temp;
+        
+        System.out.println("after swapping-");
+        System.out.println(a);
+        System.out.println(b);
+        
+        
+    }
+    
+}
